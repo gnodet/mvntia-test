5dc6d953d20ad896aab3a23ebb909e84bf48f049
Guillaume Nodet <gnodet@gmail.com>
2021-02-01 16:25:54 +0100

    Test impl change

--- testmm-m2/src/main/java/org/foo/impl/MyImpl.java
+++ testmm-m2/src/main/java/org/foo/impl/MyImpl.java
@@ -7,7 +7,6 @@ public class MyImpl implements MyInterface {
 
     @Override
     public String greet(String who) {
-        System.out.println("Test");
         return MyHelper.sayHello(who);
     }
 
